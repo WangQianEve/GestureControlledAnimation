7920728bc4d3c9f46fb37328532ab078f30448b7
evelyn <eli_home@outlook.com>
2016-12-18 15:31:17 +0800

    new

---
+++ GestureControlledAnimation/Assets/Scripts/CubeControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using UnityEngine.UI;
+
+public class CubeControl : MonoBehaviour {
+
+  public GameObject cube;
+  public HandController hc;
+  public bool selected;
+  // Use this for initialization
+  void OnTriggerEnter(Collider c)
+  {
+   if (c.gameObject.transform.parent.name.Equals("index"))
+   {
+    selected = true;
+   }
+  }
+
+  void Start()
+  {
+	  selected = false;
+  }
+  
+  Frame currentFrame;
+  void Update()
+  {
+		if(!selected)return;
+    this.currentFrame = hc.GetFrame();  
+		foreach (var h in hc.GetFrame().Hands)
+		{
+			foreach (var f in h.Fingers)
+			{
+				if (f.Type == Finger.FingerType.TYPE_INDEX)
+			  {
+					Leap.Vector position = f.TipPosition;
+					Vector3 unityPosition = position.ToUnityScaled(false);
+					Vector3 worldPosition = hc.transform.TransformPoint(unityPosition);
+  				this.cube.transform.position = worldPosition;
+			  }
+			}
+		}
+  }
+}
